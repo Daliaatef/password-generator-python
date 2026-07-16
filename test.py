3ecfd7eb0907ced11062d36b21f6339d06f9fb20
Dalia Atef <atef77669@gmail.com>
2026-07-16 22:40:23 +0300

    Remove test files

--- test.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-st.title("append test")
-#if "users" not in st.session_state:
-st.session_state.users = []
-#users = []
-name = st.text_input("enter your name")
-if st.button("add"):
-    st.session_state.users.append(name)
-st.write("users",st.session_state.users)
